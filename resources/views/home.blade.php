3b3ba90ada0b7d7e00fae474606dc8a47d927619
ivaldyp <vivaaldy@gmail.com>
2020-04-21 11:47:18 +0700

    ubah 4 jadi 3 di dashboard atas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,7 +54,7 @@
 				<div class="col-md-12">
 					<div class="white-box">
 						<div class="row row-in">
-							<div class="col-md-3 col-sm-6 row-in-br">
+							<!-- <div class="col-md-3 col-sm-6 row-in-br">
 								<ul class="col-in">
 									<li>
 										<span class="circle circle-md bg-info"><i class="ti-user"></i></span>
@@ -65,9 +65,21 @@
 									</li>
 										
 								</ul>
+							</div> -->
+							<div class="col-md-4 col-sm-4 row-in-br">
+								<ul class="col-in">
+									<li>
+										<span class="circle circle-md bg-info"><i class="ti-user"></i></span>
+									</li>
+									<li class="col-last"><h3 class="counter text-right m-t-15">{{ $countpegawai['total'] }}</h3></li>
+									<li class="col-middle">
+										<h4>Pegawai</h4>
+									</li>
+									
+								</ul>
 							</div>
 							<a href="/bpadwebs/profil/disposisi">
-							<div class="col-md-3 col-sm-6 row-in-br">
+							<div class="col-md-4 col-sm-4 row-in-br">
 								<ul class="col-in">
 									<li>
 										<span class="circle circle-md bg-danger"><i class="ti-email"></i></span>
@@ -82,7 +94,7 @@
 							</a>
 
 							<a href="/bpadwebs/cms/content">
-							<div class="col-md-3 col-sm-6 row-in-br">
+							<div class="col-md-4 col-sm-4 row-in-br">
 								<ul class="col-in">
 									<li>
 										<span class="circle circle-md bg-success"><i class="ti-comment"></i></span>
@@ -97,7 +109,7 @@
 							</div>
 							</a>
 							
-							<div class="col-md-3 col-sm-6">
+							<!-- <div class="col-md-3 col-sm-6">
 								<ul class="col-in">
 									<li>
 										<span class="circle circle-md bg-warning"><i class="fa fa-dollar"></i></span>
@@ -107,7 +119,7 @@
 										<h4>Net Earnings</h4>
 									</li>
 								</ul>
-							 </div> 
+							</div> --> 
 						</div>
 					</div>
 				</div>
