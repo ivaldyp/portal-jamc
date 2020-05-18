01396e581f7c55fbd1f97484e5c047d9944833ef
ivaldyp <vivaaldy@gmail.com>
2020-05-18 16:30:11 +0700

    benerin lelang

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -14,257 +14,259 @@ session_start();
 
 class ContentController extends Controller
 {
-    public function berita_all()
-    {
-        $berita_list = Content_tb::
-                    where('idkat', 1)
-                    ->where('appr', 'Y')
-                    ->where('sts', 1)
-                    ->orderBy('tanggal', 'desc')
-                    ->paginate(10);
+	public function berita_all()
+	{
+		$berita_list = Content_tb::
+					where('idkat', 1)
+					->where('appr', 'Y')
+					->where('sts', 1)
+					->orderBy('tanggal', 'desc')
+					->paginate(10);
 
-        $aside_top_view = Content_tb::take(3)
-                            ->where('appr', 'Y')
-                            ->where('sts', 1)
-                            ->where('idkat', 1)
-                            ->orderBy('thits', 'desc')
-                            ->get();
+		$aside_top_view = Content_tb::take(3)
+							->where('appr', 'Y')
+							->where('sts', 1)
+							->where('idkat', 1)
+							->orderBy('thits', 'desc')
+							->get();
 
-        $aside_recent = Content_tb::take(3)
-                            ->where('appr', 'Y')
-                            ->where('sts', 1)
-                            ->where('idkat', 1)
-                            ->orderBy('tanggal', 'desc')
-                            ->get();
+		$aside_recent = Content_tb::take(3)
+							->where('appr', 'Y')
+							->where('sts', 1)
+							->where('idkat', 1)
+							->orderBy('tanggal', 'desc')
+							->get();
 
-        return view('pages.berita.berita')
-                ->with('berita_list', $berita_list)
-                ->with('aside_top_view', $aside_top_view)
-                ->with('aside_recent', $aside_recent);
-    }
+		return view('pages.berita.berita')
+				->with('berita_list', $berita_list)
+				->with('aside_top_view', $aside_top_view)
+				->with('aside_recent', $aside_recent);
+	}
 
-    public function berita_read($id)
-    {
-        $thits = Content_tb::
-                    where('ids', $id)
-                    ->first(['thits']);
+	public function berita_read($id)
+	{
+		$thits = Content_tb::
+					where('ids', $id)
+					->first(['thits']);
 
-        Content_tb::where('ids', $id)
-            ->update([
-                'thits' => $thits['thits'] + 1,
-            ]);
+		Content_tb::where('ids', $id)
+			->update([
+				'thits' => $thits['thits'] + 1,
+			]);
 
-        $berita = Content_tb::
-                    where('ids', $id)
-                    ->first();
+		$berita = Content_tb::
+					where('ids', $id)
+					->first();
 
-        $aside_top_view = Content_tb::take(3)
-                            ->where('appr', 'Y')
-                            ->where('idkat', 1)
-                            ->where('sts', 1)
-                            ->orderBy('thits', 'desc')
-                            ->get();
+		$aside_top_view = Content_tb::take(3)
+							->where('appr', 'Y')
+							->where('idkat', 1)
+							->where('sts', 1)
+							->orderBy('thits', 'desc')
+							->get();
 
-        $aside_recent = Content_tb::take(3)
-                            ->where('appr', 'Y')
-                            ->where('idkat', 1)
-                            ->where('sts', 1)
-                            ->orderBy('tanggal', 'desc')
-                            ->get();
+		$aside_recent = Content_tb::take(3)
+							->where('appr', 'Y')
+							->where('idkat', 1)
+							->where('sts', 1)
+							->orderBy('tanggal', 'desc')
+							->get();
 
-        return view('pages.berita.beritasingle')
-                ->with('berita', $berita)
-                ->with('aside_top_view', $aside_top_view)
-                ->with('aside_recent', $aside_recent);
-    }
+		return view('pages.berita.beritasingle')
+				->with('berita', $berita)
+				->with('aside_top_view', $aside_top_view)
+				->with('aside_recent', $aside_recent);
+	}
 
-    public function lelang()
-    {
-        $lelang = Content_tb::
-                    where('idkat', 23)
-                    ->where('appr', 'Y')
-                    ->where('sts', 1)
-                    ->first();
+	public function lelang()
+	{
+		$lelang = Content_tb::
+					where('idkat', 23)
+					->where('appr', 'Y')
+					->where('sts', 1)
+					->first();
 
-        $thits = Content_tb::
-                    where('ids', $lelang['ids'])
-                    ->first(['thits']);
+		if ($lelang != null) {
+			$thits = Content_tb::
+						where('ids', $lelang['ids'])
+						->first(['thits']);
 
-        Content_tb::where('ids', $lelang['ids'])
-            ->update([
-                'thits' => $thits['thits'] + 1,
-            ]);
+			Content_tb::where('ids', $lelang['ids'])
+				->update([
+					'thits' => $thits['thits'] + 1,
+				]);
+		}
 
-        return view('pages.berita.lelang')
-                ->with('berita', $lelang);
-    }
+		return view('pages.berita.lelang')
+				->with('berita', $lelang);
+	}
 
-    public function foto_all(Request $request)
-    {
-        $subkat = $request->subkategori;
-        if (is_null($subkat)) {
-            $foto_list = Content_tb::
-                    where('idkat', 5)
-                    ->where('appr', 'Y')
-                    ->where('sts', 1)
-                    ->orderBy('tanggal', 'desc')
-                    ->paginate(10);
-        } else {
-            $foto_list = Content_tb::
-                    where('idkat', 5)
-                    ->where('sts', 1)
-                    ->where('subkat', $subkat)
-                    ->where('appr', 'Y')
-                    ->orderBy('tanggal', 'desc')
-                    ->paginate(10);
-        }
-        
-        $foto_kategori = Glo_subkategori::
-                        where('idkat', 5)
-                        ->orderBy('urut_subkat', 'asc')
-                        ->get();
+	public function foto_all(Request $request)
+	{
+		$subkat = $request->subkategori;
+		if (is_null($subkat)) {
+			$foto_list = Content_tb::
+					where('idkat', 5)
+					->where('appr', 'Y')
+					->where('sts', 1)
+					->orderBy('tanggal', 'desc')
+					->paginate(10);
+		} else {
+			$foto_list = Content_tb::
+					where('idkat', 5)
+					->where('sts', 1)
+					->where('subkat', $subkat)
+					->where('appr', 'Y')
+					->orderBy('tanggal', 'desc')
+					->paginate(10);
+		}
+		
+		$foto_kategori = Glo_subkategori::
+						where('idkat', 5)
+						->orderBy('urut_subkat', 'asc')
+						->get();
 
-        $aside_recent = Content_tb::take(3)
-                            ->where('appr', 'Y')
-                            ->where('sts', 1)
-                            ->where('idkat', 5)
-                            ->orderBy('tanggal', 'desc')
-                            ->get();
+		$aside_recent = Content_tb::take(3)
+							->where('appr', 'Y')
+							->where('sts', 1)
+							->where('idkat', 5)
+							->orderBy('tanggal', 'desc')
+							->get();
 
-        return view('pages.foto.foto')
-                ->with('foto_list', $foto_list)
-                ->with('foto_kategori', $foto_kategori)
-                ->with('aside_recent', $aside_recent)
-                ->with('subkat', $subkat);
-    }
+		return view('pages.foto.foto')
+				->with('foto_list', $foto_list)
+				->with('foto_kategori', $foto_kategori)
+				->with('aside_recent', $aside_recent)
+				->with('subkat', $subkat);
+	}
 
-    public function foto_open($id)
-    {   
-        $foto = Content_tb::
-                    where('ids', $id)
-                    ->first();
+	public function foto_open($id)
+	{   
+		$foto = Content_tb::
+					where('ids', $id)
+					->first();
 
-        $aside_recent = Content_tb::take(3)
-                            ->where('appr', 'Y')
-                            ->where('idkat', 5)
-                            ->orderBy('tanggal', 'desc')
-                            ->get();
+		$aside_recent = Content_tb::take(3)
+							->where('appr', 'Y')
+							->where('idkat', 5)
+							->orderBy('tanggal', 'desc')
+							->get();
 
-        return view('pages.foto.fotosingle')
-                ->with('foto', $foto)
-                ->with('aside_recent', $aside_recent);
-    }
+		return view('pages.foto.fotosingle')
+				->with('foto', $foto)
+				->with('aside_recent', $aside_recent);
+	}
 
-    public function video_all()
-    {
-        $video_list = Content_tb::
-                    where('idkat', 12)
-                    ->where('appr', 'Y')
-                    ->where('sts', 1)
-                    ->orderBy('tanggal', 'desc')
-                    ->paginate(15);
+	public function video_all()
+	{
+		$video_list = Content_tb::
+					where('idkat', 12)
+					->where('appr', 'Y')
+					->where('sts', 1)
+					->orderBy('tanggal', 'desc')
+					->paginate(15);
 
-        $aside_top_view = Content_tb::take(3)
-                            ->where('appr', 'Y')
-                            ->where('idkat', 12)
-                            ->orderBy('thits', 'desc')
-                            ->get();
+		$aside_top_view = Content_tb::take(3)
+							->where('appr', 'Y')
+							->where('idkat', 12)
+							->orderBy('thits', 'desc')
+							->get();
 
-        return view('pages.video.video')
-                ->with('video_list', $video_list)
-                ->with('aside_top_view', $aside_top_view);
-    }
+		return view('pages.video.video')
+				->with('video_list', $video_list)
+				->with('aside_top_view', $aside_top_view);
+	}
 
-    public function video_open($id)
-    {
-        $video = Content_tb::
-                    where('ids', $id)
-                    ->first();
+	public function video_open($id)
+	{
+		$video = Content_tb::
+					where('ids', $id)
+					->first();
 
-        $aside_top_view = Content_tb::take(3)
-                            ->where('appr', 'Y')
-                            ->where('idkat', 12)
-                            ->orderBy('thits', 'desc')
-                            ->get();
+		$aside_top_view = Content_tb::take(3)
+							->where('appr', 'Y')
+							->where('idkat', 12)
+							->orderBy('thits', 'desc')
+							->get();
 
-        return view('pages.video.videosingle')
-                ->with('video', $video)
-                ->with('aside_top_view', $aside_top_view);
-    }
+		return view('pages.video.videosingle')
+				->with('video', $video)
+				->with('aside_top_view', $aside_top_view);
+	}
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index()
+	{
+		//
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function create()
+	{
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(Request $request)
+	{
+		//
+	}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show($id)
+	{
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, $id)
+	{
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
 }
--- resources/views/pages/berita/lelang.blade.php
+++ resources/views/pages/berita/lelang.blade.php
@@ -3,15 +3,17 @@
 @section('content')
 
 <?php 
-	if (file_exists(config('app.openfileimgberita') . $berita['tfile'])) {
-		$fullpath = config('app.openfileimgberitafull') . $berita['tfile'];
-	} else {
-		$fullpath = 'http://bpad.jakarta.go.id/images/cms/1.20.512/1/file/' . $berita['tfile'];
+	if ($berita != null) {
+		if (file_exists(config('app.openfileimgberita') . $berita['tfile'])) {
+			$fullpath = config('app.openfileimgberitafull') . $berita['tfile'];
+		} else {
+			$fullpath = 'http://bpad.jakarta.go.id/images/cms/1.20.512/1/file/' . $berita['tfile'];
+		}
+		
+		$originalDate = explode(" ", $berita['tanggal']);
+		$newTime = explode(":", $originalDate[1]);
+		$newDate = date("d F Y", strtotime($originalDate[0]));
 	}
-	
-	$originalDate = explode(" ", $berita['tanggal']);
-	$newTime = explode(":", $originalDate[1]);
-	$newDate = date("d F Y", strtotime($originalDate[0]));
 ?>
 
 <!-- SECTION -->
@@ -24,33 +26,38 @@
 			<main id="main" class="col-md-9">
 				<!-- article -->
 				<div class="article">
-					<!-- article title -->
-					<h2 class="article-title ">{{ $berita['judul'] }}</h2><br>
-					<!-- /article title -->
-					<hr>
-					<!-- article meta -->
-					<ul style="list-style: none; padding: 0;" class="article-meta">
-						<i class="fa fa-user"></i> oleh {{ $berita['editor'] }}, {{ $newDate }}
-						<span class="pull-right">
-							<i class="fa fa-eye"></i> {{ $berita['thits'] }} views
-						</span>
-					</ul>
-					<!-- /article meta -->	
-					<hr>
-					<!-- article img -->
-					<div class="article-img">
-						<img src="<?php echo $fullpath; ?>" alt="">
-					</div>
-					<!-- article img -->
-
-					<!-- article content -->
-					<div class="article-content">
-						<div>
-							{!! html_entity_decode($berita['isi2']) !!}
+					@if($berita == null)
+						<h1>Tidak ada lelang</h1>
+					@else 
+						<!-- article title -->
+						<h2 class="article-title ">{{ $berita['judul'] }}</h2><br>
+						<!-- /article title -->
+						<hr>
+						<!-- article meta -->
+						<ul style="list-style: none; padding: 0;" class="article-meta">
+							<i class="fa fa-user"></i> oleh {{ $berita['editor'] }}, {{ $newDate }}
+							<span class="pull-right">
+								<i class="fa fa-eye"></i> {{ $berita['thits'] }} views
+							</span>
+						</ul>
+						<!-- /article meta -->	
+						<hr>
+						<!-- article img -->
+						<div class="article-img">
+							<img src="<?php echo $fullpath; ?>" alt="">
 						</div>
-					</div>
-					<br>
-					<!-- /article content -->
+						<!-- article img -->
+
+						<!-- article content -->
+						<div class="article-content">
+							<div>
+								{!! html_entity_decode($berita['isi2']) !!}
+							</div>
+						</div>
+						<br>
+						<!-- /article content -->
+					@endif
+						
 					<a href="{{ url('/') }}"><strong><i class="fa fa-arrow-left"></i> Kembali ke halaman utama</strong></a>
 				</div>					
 			</main>
