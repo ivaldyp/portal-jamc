07ff070a69a787e7a21ad149ae1929ebb3eee74d
Valdy Putra <vivaaldy@gmail.com>
2023-01-20 17:55:57 +0700

    menu baru layanan

--- resources/views/layouts/jamcportal.blade.php
+++ resources/views/layouts/jamcportal.blade.php
@@ -133,12 +133,19 @@
                                     
                                     <li class="s-header-v2__nav-item"><a href="{{ url('/') }}" class="s-header-v2__nav-link -is-active">BERANDA</a></li>
                                     <li class="s-header-v2__nav-item"><a href="{{ url('/profil') }}" class="s-header-v2__nav-link">Profil</a></li>
-                                    <!-- <li class="s-header-v2__nav-item"><a href="#section-layanan" class="s-header-v2__nav-link">Layanan</a></li> -->
+                                    <li class="dropdown s-header-v2__nav-item s-header-v2__dropdown-on-hover">
+                                        <a href="javascript:void(0);" class="dropdown-toggle s-header-v2__nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Layanan <span class="g-font-size-10--xs g-margin-l-5--xs ti-angle-down"></span></a>
+                                        <ul class="dropdown-menu s-header-v2__dropdown-menu">
+                                            <li><a href="{{ asset('img/others/sewa-img.jpg') }}" target="_blank" class="s-header-v2__dropdown-menu-link">Sewa</a></li>
+                                            <li><a href="javascript:void(0);" class="s-header-v2__dropdown-menu-link">-- Coming Soon --</a></li>
+                                            <li><a href="javascript:void(0);" class="s-header-v2__dropdown-menu-link">-- Coming Soon --</a></li>
+                                        </ul>
+                                    </li>
                                     
                                     <li class="dropdown s-header-v2__nav-item s-header-v2__dropdown-on-hover">
                                         <a href="javascript:void(0);" class="dropdown-toggle s-header-v2__nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Konten <span class="g-font-size-10--xs g-margin-l-5--xs ti-angle-down"></span></a>
                                         <ul class="dropdown-menu s-header-v2__dropdown-menu">
-                                            <li><a href="/{{config('app.name')}}#section-layanan" class="s-header-v2__dropdown-menu-link header-layanan">Layanan</a></li>
+                                            {{-- <li><a href="/{{config('app.name')}}#section-layanan" class="s-header-v2__dropdown-menu-link header-layanan">Layanan</a></li> --}}
                                             <li><a href="{{ url('/konten/berita') }}" class="s-header-v2__dropdown-menu-link">Berita</a></li>
                                             <li><a href="{{ url('/konten/galeri') }}" class="s-header-v2__dropdown-menu-link">Galeri Foto</a></li>
                                         </ul>
